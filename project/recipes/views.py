18218df969db42c3029008560daf0ac1c063644c
Sally Park <parkyngj@gmail.com>
2017-04-17 15:52:43 -0700

    Add recipes folder within projects for blueprinting

---
+++ project/recipes/views.py
@@ -0,0 +1,11 @@
+#import
+# from . import app
+from flask import render_template, Blueprint
+
+#config
+recipes_blueprint = Blueprint('recipes', __name__, template_folder='templates')
+
+#routes
+@recipes_blueprint.route('/')
+def index():
+  return render_template('index.html')
