e5ceb1d804feeeb0bece6cc6f21ba72cd9ed2de8
Sally Park <parkyngj@gmail.com>
2017-04-18 06:33:41 -0700

    Modify project init file to include and register blueprints

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,31 @@
+'''
+Below creates an instance of flask, loads the configuration, and points to a single view file.
+'''
+# imports
+# from flask import Flask
+
+# config
+# app = Flask(__name__)
+# from . import views
+
+'''
+After using Blueprint to structure our project, we need to also add the registration of each Blueprint with the Flask object (app).
+'''
 # imports
 from flask import Flask
 
 # config
 app = Flask(__name__)
-from . import views
+ # , instance_relative_config=True)
+# app.config.from_pyfile('flask.cfg')
+
+# blueprints
+from project.recipes.views import recipes_blueprintfrom project.users.views import users_blueprint
+
+# register the blueprints
+app.register_blueprint(users_blueprint)
+app.register_blueprint(recipes_blueprint)
+
+'''
+We have registered two Blueprints and they each have their own views that they define.
+'''
