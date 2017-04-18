e99d2c5e16b7b280a7935f499fecfdd0a17d64d0
Sally Park <parkyngj@gmail.com>
2017-04-18 09:22:56 -0700

    Add Recipe model to project models.py

---
+++ project/models.py
@@ -0,0 +1,21 @@
+'''
+Define all our models in here for our Flask app.
+Need to import the 'db' object that we created in ../project/__init__.py
+'''
+
+from project import db
+
+class Recipe(db.Model):
+
+    __tablename__ = "recipes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_title = db.Column(db.String, nullable=False)
+    recipe_description = db.Column(db.String, nullable=False)
+
+    def __init__(self, title, description):
+        self.recipe_title = title
+        self.recipe_description = description
+
+    def __repr__(self):
+        return '<title {}'.format(self.name)
