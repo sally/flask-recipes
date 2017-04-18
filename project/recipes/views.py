d44fb1382c5f37b12e41cd40a1aafa2440122cd6
Sally Park <parkyngj@gmail.com>
2017-04-18 11:11:18 -0700

    Implement template inheritance with layout and recipes.html

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -1,6 +1,7 @@
 #import
 # from . import app
 from flask import render_template, Blueprint
+from project.models import Recipe
 
 #config
 recipes_blueprint = Blueprint('recipes', __name__, template_folder='templates')
@@ -8,4 +9,5 @@ recipes_blueprint = Blueprint('recipes', __name__, template_folder='templates')
 #routes
 @recipes_blueprint.route('/')
 def index():
-  return render_template('index.html')
+  all_recipes = Recipe.query.all()
+  return render_template('recipes.html', recipes=all_recipes)
