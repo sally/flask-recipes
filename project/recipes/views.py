afe88bd3c455bd15fc89babcd190cc480ac5d2db
Sally Park <parkyngj@gmail.com>
2017-04-19 13:25:00 -0700

    Add routes for get/post recipes

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -1,13 +1,31 @@
-#import
+# import
 # from . import app
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, flash, request, redirect, url_for
+from project import db
 from project.models import Recipe
+from forms import AddRecipeForm, flash_errors
 
-#config
+# config
 recipes_blueprint = Blueprint('recipes', __name__, template_folder='templates')
 
-#routes
+# routes
 @recipes_blueprint.route('/')
 def index():
   all_recipes = Recipe.query.all()
   return render_template('recipes.html', recipes=all_recipes)
+
+@recipes_blueprint.route('/add', methods=['GET', 'POST'])
+def add_recipe():
+  form = AddRecipeForm(request.form)
+  if request.method == 'POST':
+    if form.validate_on_submit():
+      new_recipe = Recipe(form.recipe_title.data, form.recipe_description.data)
+      db.session.add(new_recipe)
+      db.session.commit()
+      flash('New recipe, {}, added!'.format(new_recipe.recipe_title), 'success')
+      return redirect(url_for('recipes.index'))
+    else:
+      flash_errors(form)
+      flash('Error - Recipe was not added.', 'error')
+
+  return render_template('add_recipe.html', form=form)
