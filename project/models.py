8ed9e5af30b98f50ed47e1e28885f582cba43a62
Sally Park <parkyngj@gmail.com>
2017-04-19 13:37:09 -0700

    Add code for hashing user passwords

--- project/models.py
+++ project/models.py
@@ -26,11 +26,26 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String, unique=True, nullable=False)
-    password_plaintext = db.Column(db.String, nullable=False)
+    _password = db.Column(db.(Binary(60)), nullable=False)
+    authenticated = db.Column(db.Boolean, default=False)
 
     def __init__(self, email, password_plaintext):
         self.email = email
         self.password_plaintext = password_plaintext
+        self.authenticated = False
+
+    @password.setter
+    def set_password(self, plaintext_password):
+        self._password = bcrypt.generate_password_hash(plaintext_password)
+
+    @hybrid_property
+    def password(self)
+    return self._password
+
+    @hybrid_method
+    def is_correct_password(self, plaintext_password):
+        return bcrypt.check_password_hash(self.password, self.plaintext_password)
 
     def __repr__(self):
-        return "<User {0}".format(self.name)
+        return "
+        <User {0}".format(self.name)
