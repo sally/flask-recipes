0a460568003d706b103ba6d2a8d6f696ced7fe6f
Sally Park <parkyngj@gmail.com>
2017-04-19 15:35:01 -0700

    Define flash_errors in users forms.py

--- project/users/forms.py
+++ project/users/forms.py
@@ -6,3 +6,11 @@ class RegisterForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email(), Length(min=6, max=40)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)])
     confirm = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash("Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ))
